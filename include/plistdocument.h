a5ddfdc10e6743c1ce9c4fdec9d7386fa36604f3
zshalex <zshalex@gmail.com>
2013-07-18 15:10:54 +0800

    refactor the plistdocument

--- include/plistdocument.h
+++ include/plistdocument.h
@@ -5,16 +5,19 @@
 #include <QMap>
 #include <QDomDocument>
 #include <QVariant>
+#include <QStringList>
 
-#define ROOT_NODE "plist"
-#define DICT_NODE "dict"
-#define STRING_NODE "string"
-#define ARRAY_NODE "array"
-#define KEY_NODE "key"
-#define INTEGER_NODE "integer"
-#define REAL_NODE "real"
-#define DATE_NODE "date"
-#define DATA_NODE "deta"
+#define ROOT_NODE_NAME    "plist"
+#define DICT_NODE_NAME    "dict"
+#define STRING_NODE_NAME  "string"
+#define ARRAY_NODE_NAME   "array"
+#define KEY_NODE_NAME     "key"
+#define INTEGER_NODE_NAME "integer"
+#define REAL_NODE_NAME    "real"
+#define DATE_NODE_NAME    "date"
+#define DATA_NODE_NAME    "deta"
+#define TRUE_NODE_NAME    "true"
+#define FALSE_NODE_NAME   "false"
 
 typedef QMap<QString, QVariant> PListDict;
 typedef QList<QVariant> PListArray;
@@ -23,26 +26,41 @@ class PListDocument : public QObject
 {
     Q_OBJECT
 public:
+    enum NODE_TYPE {
+        DICT_NODE = 0,
+        ARRAY_NODE,
+        STRING_NODE,
+        INTEGER_NODE,
+        REAL_NODE,
+        DATE_NODE,
+        DATA_NODE,
+        TRUE_NODE,
+        FALSE_NODE
+    };
+
     explicit PListDocument(QObject *parent = 0);
     ~PListDocument();
 
-    const PListDict & dict() const;
+    const QVariant & root() const;
 
-    bool load(QString filename);
-    bool save(QString filename);
-private:
-    QString getType(QDomElement &element, bool direct = false);
+    void setRoot(const QVariant &root);
 
-    bool loadDict(QDomElement &element, PListDict &dict);
-    bool loadArray(QDomElement &element, PListArray &list);
-    bool loadString(QDomElement &element, QString &string);
-    bool loadInteger(QDomElement &element, int &value);
-    bool loadReal(QDomElement &element, float &value);
-    bool loadDate(QDomElement &element, QDateTime &date);
-    bool loadData(QDomElement &element, QByteArray &data);
-//    bool load
+    bool load(const QString &filename);
+    bool save(const QString &filename);
 private:
+    QVariant loadValue(const QDomElement &element);
+
+    bool loadDict(const QDomElement &element, PListDict &dict);
+    bool loadArray(const QDomElement &element, PListArray &list);
+    bool loadString(const QDomElement &element, QString &string);
+    bool loadInteger(const QDomElement &element, int &value);
+    bool loadReal(const QDomElement &element, qreal &value);
+    bool loadDate(const QDomElement &element, QDateTime &date);
+    bool loadData(const QDomElement &element, QByteArray &data);
+private:
+    static QStringList m_nodeMap;
     PListDict m_dict;
+    QVariant m_root;
 };
 
 #endif // PLISTDOCUMENT_H
--- src/plistdocument.cpp
+++ src/plistdocument.cpp
@@ -4,6 +4,11 @@
 
 #include "plistdocument.h"
 
+QStringList PListDocument::m_nodeMap(
+        QStringList() << DICT_NODE_NAME << ARRAY_NODE_NAME
+        << STRING_NODE_NAME << INTEGER_NODE_NAME << REAL_NODE_NAME
+        << DATE_NODE_NAME << DATA_NODE_NAME << TRUE_NODE_NAME << FALSE_NODE_NAME);
+
 PListDocument::PListDocument(QObject *parent) :
     QObject(parent)
 {
@@ -14,8 +19,19 @@ PListDocument::~PListDocument()
 
 }
 
-bool PListDocument::load(QString filename)
+const QVariant &PListDocument::root() const
 {
+    return m_root;
+}
+
+void PListDocument::setRoot(const QVariant &root)
+{
+    m_root = root;
+}
+
+bool PListDocument::load(const QString &filename)
+{
+    m_root.clear();
     QDomDocument doc;
     QFile file(filename);
     if (!file.open(QIODevice::ReadOnly))
@@ -28,193 +44,157 @@ bool PListDocument::load(QString filename)
 
     QDomElement root = doc.documentElement();
 
-    if (root.nodeName() != ROOT_NODE)
+    if (root.nodeName() != ROOT_NODE_NAME)
         return false;
 
     QDomElement element = root.firstChildElement();
 
-    return loadDict(element,m_dict);
+    m_root = loadValue(element);
+
+    return !m_root.isNull();
 }
 
-bool PListDocument::save(QString filename)
+bool PListDocument::save(const QString &)
 {
+    return false;
 }
 
-QString PListDocument::getType(QDomElement &element, bool direct)
+QVariant PListDocument::loadValue(const QDomElement &element)
 {
+    QVariant result;
     if (element.isNull())
-        return "";
-    if (direct) {
-        return element.nodeName();
-    } else {
-        QDomElement value = element.nextSiblingElement();
-        if (value.isNull())
-            return "";
-        return value.nodeName();
+        return result;
+    QString type = element.nodeName();
+
+    switch(m_nodeMap.indexOf(type)) {
+    case DICT_NODE: {
+        PListDict dict;
+        if (loadDict(element,dict))
+            result = dict;
+        break;
     }
+    case ARRAY_NODE: {
+        PListArray array;
+        if (loadArray(element,array))
+            result = array;
+        break;
+    }
+    case STRING_NODE: {
+        QString str;
+        if (loadString(element,str))
+            result = str;
+        break;
+    }
+    case INTEGER_NODE: {
+        int value;
+        if (loadInteger(element,value))
+            result = value;
+        break;
+    }
+    case REAL_NODE: {
+        qreal value;
+        if (loadReal(element,value))
+            result = value;
+        break;
+    }
+    case DATE_NODE: {
+        QDateTime date;
+        if (loadDate(element,date))
+            result = date;
+        break;
+    }
+    case TRUE_NODE: {
+        result = true;
+        break;
+    }
+    case FALSE_NODE: {
+        result = false;
+        break;
+    }
+    default:
+        break;
+    }
+    return result;
 }
 
-bool PListDocument::loadDict(QDomElement &element, PListDict &dict)
+bool PListDocument::loadDict(const QDomElement &element, PListDict &dict)
 {
-    if (element.isNull() || element.nodeName() != DICT_NODE)
+    if (element.isNull() || element.nodeName() != DICT_NODE_NAME)
         return false;
 
-    QDomElement n = element.firstChildElement(KEY_NODE);
-    QString type;
-    QString key;
+    QDomElement n = element.firstChildElement(KEY_NODE_NAME);
+    QVariant var;
     QDomElement value;
-    bool result;
     dict.clear();
     while (!n.isNull()) {
-        type = getType(n);
-        if (type.isEmpty())
-            return false;
-
         value = n.nextSiblingElement();
-        key = n.firstChild().nodeValue();
-
-        if (type == STRING_NODE) {
-            QString str;
-            result = loadString(value,str);
-            if (result) {
-                dict.insert(key, str);
-            }
-        } else if (type == DICT_NODE) {
-            PListDict subDict;
-            result = loadDict(value,subDict);
-            if (result)
-                dict.insert(key,subDict);
-        } else if (type == ARRAY_NODE) {
-            PListArray array;
-            result = loadArray(value,array);
-            if (result)
-                dict.insert(key,array);
-        } else if (type == INTEGER_NODE) {
-            int i;
-            result = loadInteger(value, i);
-            if(result)
-                dict.insert(key,i);
-        } else if (type == REAL_NODE) {
-            float f;
-            result = loadReal(value, f);
-            if(result)
-                dict.insert(key,f);
-        } else if (type == DATE_NODE) {
-            QDateTime date;
-            result = loadDate(value, date);
-            if(result)
-                dict.insert(key,date);
-        } else if (type == DATA_NODE) {
-            QByteArray data;
-            result = loadData(value, data);
-            if(result)
-                dict.insert(key,data);
-        }
-        n = n.nextSiblingElement(KEY_NODE);
+        if (value.isNull())
+            return false;
+        var = loadValue(value);
+        if (var.isNull())
+            return false;
+        dict.insert(n.firstChild().nodeValue(),
+                    var);
+        n = n.nextSiblingElement(KEY_NODE_NAME);
     }
     return true;
 }
 
-bool PListDocument::loadArray(QDomElement &element, PListArray &list)
+bool PListDocument::loadArray(const QDomElement &element, PListArray &list)
 {
-    if (element.isNull() || element.nodeName() != ARRAY_NODE)
+    if (element.isNull() || element.nodeName() != ARRAY_NODE_NAME)
         return false;
 
     list.clear();
     QDomElement n = element.firstChildElement();
-    QString type;
-    bool result = false;
+    QVariant var;
     while (!n.isNull()) {
-        type = getType(n,true);
-        if (type.isEmpty())
-            break;
-
-        if (type == STRING_NODE) {
-            QString str;
-            result = loadString(n,str);
-            if (result)
-                list.append(str);
-        } else if (type == DICT_NODE) {
-            PListDict dict;
-            result = loadDict(n,dict);
-            if (result)
-                list.append(dict);
-        } else if (type == ARRAY_NODE) {
-            PListArray array;
-            result = loadArray(n,array);
-            if (result)
-                list.append(array);
-        } else if (type == INTEGER_NODE) {
-            int i;
-            result = loadInteger(n, i);
-            if(result)
-                list.append(i);
-        } else if (type == REAL_NODE) {
-            float f;
-            result = loadReal(n, f);
-            if(result)
-                list.append(f);
-        } else if (type == DATE_NODE) {
-            QDateTime date;
-            result = loadDate(n, date);
-            if(result)
-                list.append(date);
-        }
-        else if (type == DATA_NODE) {
-            QByteArray data;
-            result = loadData(n, data);
-            if(result)
-                list.append(data);
-        }
+        var = loadValue(n);
+        if (var.isNull())
+            return false;
+        list.append(var);
         n = n.nextSiblingElement();
     }
     return true;
 }
 
-bool PListDocument::loadString(QDomElement &element, QString &string)
+bool PListDocument::loadString(const QDomElement &element, QString &string)
 {
-    if (element.isNull() || element.nodeName() != STRING_NODE)
+    if (element.isNull() || element.nodeName() != STRING_NODE_NAME)
         return false;
     string = element.firstChild().nodeValue();
     return true;
 }
 
-bool PListDocument::loadInteger(QDomElement &element, int &value)
+bool PListDocument::loadInteger(const QDomElement &element, int &value)
 {
-    if (element.isNull() || element.nodeName() != STRING_NODE)
+    if (element.isNull() || element.nodeName() != INTEGER_NODE_NAME)
         return false;
     value = element.firstChild().nodeValue().toInt();
     return true;
 }
 
-bool PListDocument::loadReal(QDomElement &element, float &value)
+bool PListDocument::loadReal(const QDomElement &element, qreal &value)
 {
-    if (element.isNull() || element.nodeName() != STRING_NODE)
+    if (element.isNull() || element.nodeName() != REAL_NODE_NAME)
         return false;
-    value = element.firstChild().nodeValue().toFloat();
+    value = element.firstChild().nodeValue().toDouble();
     return true;
 }
 
-bool PListDocument::loadDate(QDomElement &element, QDateTime &date)
+bool PListDocument::loadDate(const QDomElement &element, QDateTime &date)
 {
-    if (element.isNull() || element.nodeName() != STRING_NODE)
+    if (element.isNull() || element.nodeName() != DATE_NODE_NAME)
         return false;
     date = QDateTime::fromString(element.firstChild().nodeValue(),"yyyy-MM-dd hh:mm:ss");
     return true;
 }
 
-bool PListDocument::loadData(QDomElement &element, QByteArray &data)
+bool PListDocument::loadData(const QDomElement &element, QByteArray &data)
 {
-    if (element.isNull() || element.nodeName() != STRING_NODE)
+    if (element.isNull() || element.nodeName() != DATA_NODE_NAME)
         return false;
     data = QByteArray::fromBase64(
-                element.firstChild().nodeValue().toAscii());
+                element.firstChild().nodeValue().toLatin1());
     return true;
 }
-
-const PListDict & PListDocument::dict() const
-{
-    return m_dict;
-}
-
-
